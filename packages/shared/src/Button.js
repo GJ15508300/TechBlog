27ab517b5fea4f752f3175e76c59ad0583e9a7fb
GJ <jagadeesh15508300@gmail.com>
2024-10-31 12:46:23 +0530

    web build test success

---
+++ packages/shared/src/Button.js
@@ -0,0 +1,24 @@
+// packages/shared/src/Button.js
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Button = ({ onPress, title }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.text}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#007bff',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default Button;
